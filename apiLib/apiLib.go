ea120edf8dc074a71294ca655f196339109b1745
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2024-12-01 16:51:20 +0100

    added new funcs GetList and PrintList

--- apiLib/apiLib.go
+++ apiLib/apiLib.go
@@ -1,5 +1,12 @@
 package apiLib
 
+import (
+	"fmt"
+	"os"
+	"github.com/goccy/go-yaml"
+)
+
+
 func VerifyCmd(cmdStr string) (bool) {
     cmdList := []string{"list", "get", "add", "upd", "rm"}
     for i:=0; i< len(cmdList); i++ {
@@ -11,6 +18,7 @@ func VerifyCmd(cmdStr string) (bool) {
 }
 
 func VerifyApp(appStr string) (bool) {
+
     appList := []string{"namcheap", "cloudflare", "nchsb", "*", "all"}
     for i:=0; i< len(appList); i++ {
         if appStr == appList[i] {
@@ -20,4 +28,43 @@ func VerifyApp(appStr string) (bool) {
     return false
 }
 
+type prov struct {
+	Nam string
+	Val provVal
+}
 
+type provVal struct {
+	Token string
+}
+
+type provMap map[string]string
+
+func GetList(yamlFil string) (list map[string]provMap, err error) {
+
+//	var ProvList []prov
+
+	list = make(map[string]provMap)
+
+	ldat, err := os.ReadFile(yamlFil)
+	if err != nil {return list, fmt.Errorf("Read List: %v", err)}
+
+	fmt.Printf("dbg -- %s\n", ldat)
+
+	err = yaml.Unmarshal(ldat,&list)
+	if err != nil {return list, fmt.Errorf("UnMarshal: %v", err)}
+
+	return list, nil
+}
+
+
+func PrintList(list map[string]provMap) {
+
+	fmt.Printf("*** api list: %d providers ***\n", len(list))
+	for nam, val := range list {
+		fmt.Printf("provider: %s\n", nam)
+		for key, kval := range val {
+			fmt.Printf(" %s : %s\n", key, kval)
+		}
+	}
+	fmt.Printf("*** end api list providers ***\n")
+}
